03cb6705e4f888c4b9799ab954452a3f993d4345
chris <christopher.narayanan@uzh.ch>
2022-12-08 16:54:53 +0100

    thinking about classes and their variables

--- modules/identifier.py
+++ modules/identifier.py
@@ -2,8 +2,17 @@ import random
 import string
 
 random.seed("ibf")
-class identifier:
+class Identifier(object):
+    instance = None
+
+
     def __init__(self):
-        self.already_tooken_identifiers=[]
-    def generate_identifier(length:int =12):
-        return "".join(random.choice(string.ascii_letters) for i in range(length))
+        self.__already_tooken_identifiers=[]
+
+    def generate_identifier(self,length:int =12):
+        random_identifier="".join(random.choice(string.ascii_letters) for i in range(length))
+
+        while random_identifier in self.__already_tooken_identifiers:
+            random_identifier="".join(random.choice(string.ascii_letters) for i in range(length))
+        self.__already_tooken_identifiers.append(random_identifier)
+        return random_identifier
--- modules/student.py
+++ modules/student.py
@@ -1,6 +1,7 @@
-class Student:
-    def __init__(self,matriculation_Number,first_name,last_name):
+class Student():
+    def __init__(self,matriculation_Number,first_name,last_name,id):
         self.__matriculation_num=matriculation_Number
         self.__first_name=first_name
         self.__last_name=last_name
+        self.__id=id
 
