b8f0aa850424f28db5c297bff6df156fb0c6bda2
Christopher Narayanan <93202854+Queentaker@users.noreply.github.com>
2023-01-13 15:14:36 +0100

    correcting spelling mistake

--- modules/identifier.py
+++ modules/identifier.py
@@ -2,6 +2,6 @@ import random
 import string
 
 random.seed("ibf")
-#changed the identifier function into a class
+
 def generate_identifier(length=12):
     return "".join(random.choice(string.ascii_letters) for i in range(length))
